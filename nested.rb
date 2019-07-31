d8bb3a4bf70d08e83b920611bb7bf49080b6376e
Ashton Williams <ashtonwilliams1996@gmail.com>
2019-07-31 17:13:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,18 @@
-
+	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
 def hopper
 	programmer_hash = 
  		{
